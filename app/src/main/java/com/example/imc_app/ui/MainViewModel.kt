6e8c33aae690e06e22f3bc8b2c3c03029498df8d
Elisabeth Farias <elisabethfarias7@gmail.com>
2021-06-17 22:21:01 -0300

    Updated viewModel

--- app/src/main/java/com/example/imc_app/ui/MainViewModel.kt
+++ app/src/main/java/com/example/imc_app/ui/MainViewModel.kt
@@ -25,8 +25,18 @@ class MainViewModel : ViewModel() {
 
    fun checkIMC(imcValue: Double) : String {
        var msg = ""
-        if(imcValue <= 28.30) {
-            msg = "$imcValue acima do peso"
+        if(imcValue < 18.50) {
+            msg = "$imcValue Abaixo do peso"
+        } else if(imcValue > 18.50 && imcValue <= 24.90){
+            msg = "$imcValue Peso normal"
+        } else if(imcValue > 25 && imcValue <= 29.90) {
+            msg = "$imcValue Acima do peso"
+        } else if(imcValue > 30.90 && imcValue <= 34.90) {
+            msg = "$imcValue Obesidade Grau I"
+        } else if(imcValue > 35.0 && imcValue <= 40.0) {
+            msg = "$imcValue Obesidade Grau II"
+        } else if(imcValue >= 40.0) {
+            msg = "$imcValue Obesidade Grau III"
         }
        return msg
    }
